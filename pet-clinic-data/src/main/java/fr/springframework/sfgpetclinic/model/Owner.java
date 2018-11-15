c34ff84808da4913a8e362c9f09fc7dc0c303afe
arnaud.goureman <arnaud.goureman@selsia.fr>
2018-11-15 16:40:32 +0100

    added visits, updated owner with pet relationship. Closes #26

--- pet-clinic-data/src/main/java/fr/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/fr/springframework/sfgpetclinic/model/Owner.java
@@ -1,5 +1,13 @@
 package fr.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Owner extends Person
 {
+	private Set<Pet> pets;
 }

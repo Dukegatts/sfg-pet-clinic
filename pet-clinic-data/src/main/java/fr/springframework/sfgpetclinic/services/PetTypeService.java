f3c3a776c385642a37ed2b9384ff2e664fa4364e
arnaud.goureman <arnaud.goureman@selsia.fr>
2018-11-15 16:49:56 +0100

    added PetTypeService. Closes #29

---
+++ pet-clinic-data/src/main/java/fr/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package fr.springframework.sfgpetclinic.services;
+
+import fr.springframework.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long>
+{
+}
---
+++ pet-clinic-data/src/main/java/fr/springframework/sfgpetclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,43 @@
+package fr.springframework.sfgpetclinic.services.map;
+
+import java.util.Set;
+
+import org.springframework.stereotype.Service;
+
+import fr.springframework.sfgpetclinic.model.PetType;
+import fr.springframework.sfgpetclinic.services.PetTypeService;
+
+@Service
+public class PetTypeServiceMap
+	extends AbstractMapService<PetType, Long> implements PetTypeService
+{
+	@Override
+	public Set<PetType> findAll()
+	{
+		return super.findAll();
+	}
+
+	@Override
+	public void deleteById(final Long id)
+	{
+		super.deleteById(id);
+	}
+
+	@Override
+	public void delete(final PetType object)
+	{
+		super.delete(object);
+	}
+
+	@Override
+	public PetType save(final PetType object)
+	{
+		return super.save(object);
+	}
+
+	@Override
+	public PetType findById(final Long id)
+	{
+		return super.findById(id);
+	}
+}

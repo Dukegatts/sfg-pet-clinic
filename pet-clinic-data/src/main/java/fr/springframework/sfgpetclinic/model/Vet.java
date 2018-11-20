1ee9fed11b0f70aca3f1cdcc2e97a84ab3682aed
arnaud.goureman <arnaud.goureman@selsia.fr>
2018-11-20 18:04:15 +0100

    Updated vetto use JPA. Closes #37

--- pet-clinic-data/src/main/java/fr/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/fr/springframework/sfgpetclinic/model/Vet.java
@@ -2,13 +2,23 @@ package fr.springframework.sfgpetclinic.model;
 
 import java.util.HashSet;
 import java.util.Set;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
 
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "vets")
 public class Vet extends Person
 {
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "vet_specialities", joinColumns = @JoinColumn(name = "vet_id"), inverseJoinColumns = @JoinColumn(name = "speciality_id"))
 	private Set<Speciality> specialities = new HashSet<>();
 }

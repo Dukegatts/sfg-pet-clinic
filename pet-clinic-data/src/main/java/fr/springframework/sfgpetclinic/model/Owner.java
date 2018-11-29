e218f4ff89322eead4232d2b7d5072beb6a61702
arnaud.goureman <arnaud.goureman@selsia.fr>
2018-11-29 12:29:49 +0100

    Added Project Lombok to project. Closes #51

--- pet-clinic-data/src/main/java/fr/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/fr/springframework/sfgpetclinic/model/Owner.java
@@ -8,15 +8,35 @@ import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @Entity
 @Table(name = "owners")
-public class Owner extends Person
+public class Owner
+	extends Person
 {
+	@Builder
+	public Owner(final Long id,
+				 final String firstName,
+				 final String lastName,
+				 final String address,
+				 final String city,
+				 final String telephone,
+				 final Set<Pet> pets)
+	{
+		super(id, firstName, lastName);
+		this.address = address;
+		this.city = city;
+		this.telephone = telephone;
+		this.pets = pets;
+	}
+
 	@Column(name = "address")
 	private String address;
 
--- pet-clinic-data/src/main/java/fr/springframework/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/fr/springframework/sfgpetclinic/model/Person.java
@@ -4,13 +4,22 @@ import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @MappedSuperclass
 public class Person extends BaseEntity
 {
+	public Person(final Long id, final String firstName, final String lastName)
+	{
+		super(id);
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
 	@Column(name="first_name")
 	private String firstName;
 
